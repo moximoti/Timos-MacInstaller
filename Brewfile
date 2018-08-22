314f0958407a07e1072bd188e2f4ed7730b0f204
Timo Volkmann <timo.tvm@gmail.com>
2018-08-22 16:23:12 +0200

    init

---
+++ Brewfile
@@ -0,0 +1,186 @@
+tap 'caskroom/cask'
+tap 'homebrew/bundle'
+tap 'homebrew/core'
+
+
+#
+# Comment or uncomment if you want to adapt your installation
+#
+
+# OSX Terminal stuff that will make your life easier
+brew 'bash'
+brew 'coreutils'
+brew 'curl'
+brew 'git'
+brew 'git-lfs'
+brew 'n'
+brew 'trash'
+brew 'wget'
+brew 'ack'
+brew 'freetype'
+brew 'git'
+brew 'graphviz'
+brew 'imagemagick'
+brew 'libpng'
+brew 'pkg-config'
+brew 'rename'
+brew 'tmux'
+brew 'tree'
+brew 'vim'
+brew 'wget'
+brew 'jenv'
+# jEnv is a command line tool to help you forget how to set the JAVA_HOME environment variable
+
+#
+# system tools
+#
+cask 'little-snitch'
+
+cask 'osxfuse'
+# let you mount windows drives and write on it
+
+cask '1password'
+# maintain your passwords
+
+cask 'appcleaner'
+# remove apps with system setting files
+
+cask 'iterm2'
+# nicer terminal
+
+cask 'recordit'
+# record a part of the screen as gif
+
+cask 'rightfont'
+# good font management for osx
+
+cask 'fontstand'
+# try expensive fonts for 1h
+
+cask 'spectacle'
+# rearrange your OSX windows with shortcuts
+
+cask 'the-unarchiver'
+# better Unarchiver for OSX (supports more formats but works the same)
+
+# extends quicklook so it supports more formats
+cask 'qlcolorcode'
+cask 'qlimagesize'
+cask 'qlmarkdown'
+cask 'qlstephen'
+cask 'quicklook-csv'
+cask 'quicklook-json'
+cask 'webpquicklook'
+
+
+#
+# Text & Code
+#
+
+mas 'Xcode', id: 497799835
+# OSX Coding tools and IOS Simulators
+
+cask 'java'
+cask 'phpstorm'
+cask 'intellij-idea'
+cask 'sublime-text'
+cask 'atom'
+cask 'visual-studio-code'
+cask 'visual-paradigm'
+cask 'dash'
+cask 'typora'
+# most powerful ide for php and other programming languages
+
+#cask 'docker'
+#cask 'kitematic'
+cask 'vmware-fusion'
+cask 'vagrant'
+cask 'vagrant-manager'
+cask 'vagrant-vmware-utility'
+# install homestead manually
+# host for local development
+
+#
+# Internet stuff
+#
+
+cask 'google-chrome'
+cask 'firefox'
+cask 'brave'
+cask 'torbrowser'
+
+cask 'spotify'
+
+#
+# Collaboration & communication
+#
+
+cask 'gitter'
+cask 'teamviewer'
+cask 'joinme'
+cask 'skype'
+cask 'github'
+cask 'fork'
+cask 'whatsapp'
+cask 'telegram-desktop'
+cask 'discord'
+cask 'dropbox'
+cask 'box-drive'
+cask 'box-sync'
+
+# MS Office & OpenOffice
+cask 'microsoft-office'
+cask 'openoffice'
+
+#
+# Creative stuff & image tools
+#
+
+cask 'imagealpha'
+# create small transparent PNG
+
+cask 'imageoptim'
+# PNG optimization
+
+
+#
+# Multimedia & Tools
+#
+
+cask 'daisydisk'
+cask 'parsec'
+cask 'ableton-live'
+cask 'theine'
+cask 'adobe-acrobat-pro'
+cask 'adobe-creative-cloud'
+cask 'adobe-creative-cloud-cleaner-tool'
+cask 'capture-one'
+cask 'moneymoney'
+cask 'jdownloader'
+cask 'fantastical'
+cask 'cloudmounter'
+cask 'coconutbattery'
+cask 'etrecheck'
+cask 'forklift'
+cask 'gpg-suite'
+cask 'totals'
+cask 'grandtotals'
+cask 'timings'
+cask 'namechanger'
+cask 'handbreak'
+cask 'mkchromecast'
+cask 'vlc'
+cask 'lingon-x'
+cask 'kodi'
+cask 'tuneinstructor'
+cask 'imageoptim'
+cask 'quitter'
+cask 'certbot'
+cask 'paw'
+cask 'advancedcolors'
+cask 'padbury-clock'
+cask 'sketch'
+cask 'staruml'
+#cask 'keybase'
+
+
---
+++ macinstaller.sh
@@ -0,0 +1,213 @@
+#!/usr/bin/env bash
+
+#Install XCode
+echo Install xcodeutils…
+read -p "Press any key to continue... " -n1 -s
+echo  '\n'
+xcode-select --install
+
+#Install Homebrew, if not already installed
+echo Install Homebrew, and homebrew bundle…
+read -p "Press any key to continue... " -n1 -s
+echo  '\n'
+
+echo "alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'" >> ~/.bash_profile
+source ~/.bash_profile
+
+if [ ! $(which brew) ]; then
+  echo "Installing Homebrew…"
+  ruby -e "$(curl -fsSl https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+else
+  echo "Updating and cleaning Homebrew…"
+  brewup
+fi
+
+
+# Apple signin
+brew tap Homebrew/bundle
+brew install mas
+echo "Sign in with your Apple ID:"
+read APPLEID
+
+if [ -z "$APPLEID" ]; then
+  echo "Skipping AppStore Installations..."
+else 
+  mas signin $APPLEID
+fi
+
+read -p "Press any key to continue... " -n1 -s
+echo  '\n'
+
+
+#Install GNU core utilities (those that come with OS X are outdated)
+echo "Installing GNU core utilities…"
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+
+
+#Install new Bash
+echo "Installing new Bash…"
+brew install bash bash-completion
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+
+#Install and switch to Zsh
+echo "Installing Zsh…"
+brew install zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
+chsh -s /usr/local/bin/zsh #Switch default shell to Zsh
+
+#Add oh-my-zsh, theme, plugins, powerline fonts
+echo "Installing and configuring oh-my-zsh…"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+sed -i -e 's/  git/  colored-man-pages\n  git\n  python\n  sublime/g' ~/.zshrc
+
+#Add basic Zsh configuration
+echo "Configuring Zsh…"
+echo -e 'source ~/.bash_profile\n' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
+echo "export DEFAULT_USER=\`whoami\`" >> ~/.zshrc
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+
+#Install apps to /Applications
+#Each has its own command to prevent errors in remaining installed if a piece of software is already installed (a cask design decision).
+echo "Install Packages & Install bundled software to /Applications…"
+read -p "Press any key to continue... " -n1 -s
+brew bundle
+
+
+#Python time!
+brew install pyenv pyenv-virtualenv
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+echo -e 'if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi' >> ~/.zshrc
+exec "$SHELL"
+
+#Type 'pyenv install -l' to see a list of available versions for install
+#Installing the plugin below creates the ability to also use "pyenv install-latest".
+git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest
+pyenv install-latest
+# pyenv global x.x.x to set the global version of python.
+
+#Create iPython profile to work w/ pyenv-virtualenv
+#https://medium.com/@henriquebastos/the-definitive-guide-to-setup-my-python-workspace-628d68552e14
+#ipython profile create
+#curl -L http://hbn.link/hb-ipython-startup-script > ~/.ipython/profile_default/startup/00-venv-sitepackages.py
+
+
+
+#Set Git's default text editor, name, and credential helper
+echo "Configuring Git…"
+git config --global core.editor "subl -n -w"
+git config --global user.name "moximoti"
+git config --global credential.helper osxkeychain
+
+
+#Install Google SDK, update PATH, enable Zsh completion
+#echo "Installing Google SDK…"
+#brew cask install --appdir="/Applications" google-cloud-sdk
+#echo "# The next line updates PATH for the Google Cloud SDK." >> ~/.zshrc
+#echo "if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'; fi" >> ~/.zshrc
+#echo "# The next line enables shell command completion for gcloud." >> ~/.zshrc
+#echo "if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'; fi" >> ~/.zshrc
+
+# #Fix gcloud/docker integration
+# gcloud components install docker-credential-gcr
+# #EXIT TERMINAL COMPLETELY AND REOPEN
+# docker-credential-gcr configure-docker
+# screen ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
+# umount /var/lib/docker/overlay2
+# rm -rf /var/lib/docker
+# #EXIT TERMINAL COMPLETELY AND REOPEN
+
+#Clean up after Homebrew
+echo "Cleaning up…"
+brewup
+brew cleanup --force
+rm -f -r /Library/Caches/Homebrew/*
+
+
+#Set up OS tweaks
+echo "Tweaking OS settings…"
+
+#Enable right click; logging out and back in is required to take effect
+#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+#defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+#defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+#defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 2
+#defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+
+#Show hidden programs by default. Allows using ⌘+H to hide, and ⌘+Tab switching
+#defaults write com.apple.Dock showhidden -bool TRUE
+#killall Dock
+
+#Remove Dock delay
+defaults write com.apple.Dock autohide-delay -float 0
+killall Dock
+
+#Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool TRUE
+killall Finder
+
+#Show long path name on all folders
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+killall Finder
+
+#Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+killall Finder
+
+#Show file extensions in Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+killall Finder
+
+#Disable drop shadows on a screenshot
+defaults write com.apple.screencapture disable-shadow -bool TRUE
+killall SystemUIServer
+
+# Check for macOS updates daily instead of weekly
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Prevent Apps From Saving to iCloud by Default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool FALSE
+
+#Enable debug menu
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+#Change default text editor to Sublime Text 3
+#If this command doesn't work, see http://ultimatemac.com/how-to-change-default-text-editor-on-mac/ for manual instructions
+#defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add \
+#'{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
+
+
+#Finished!
+echo "Done! Don't forget to make manual changes noted at the end of the installer script."
+
+
+#Manual Installs/Steps
+#Install Pycharm CE or Pro
+#Install Docker for Mac  
+#Install Flycut clipboard - Alfred has this, but you need to buy the powerpack for $19
+
+##Enabling 'tap to click' via CLI no longer works since Sierra. Manually configure via System Preferences > Trackpad
+##Enable tap to click; logging out and back in is required to take effect
+#sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+#sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+#sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+#Configure iTerm settings
+  #Manually configure iTerm for word jumps (option + → or ←) and word deletion (option + backspace). 
+    #iTerm > Preferences > Profiles > Keys > Load Preset... > select 'Natural Text Editing'
+  #Colors
+    #iTerm > Preferences > Profiles > Colors > Color Presets... > select 'Solarized Dark'
+  #Install a patched font
+    #Download Meslo by clicking 'view raw' at https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf .
+    #Open the downloaded font and press "Install Font".
+    #iTerm > Preferences > Profiles > Text > Change Font > select '13pt Meslo LG L Regular for Powerline'
+    #Restrat iTerm2
